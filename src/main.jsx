0dd5ab9d9f8a4137f039655e507a45f4af170916
nguyencuong1010a <nguyencuong1010a@gmail.com>
2023-07-07 14:17:26 +0700

    hihi

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import App from "./App.jsx";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import store from './Redux/Store'
+import store from "./redux/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-          <App />
+        <App />
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
 );
-
